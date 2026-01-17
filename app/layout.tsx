374ea25d358ff924943b8b57eaab937a0056ce54
Huajun Yuan <36772310+KhronosLove@users.noreply.github.com>
2026-01-17 23:41:15 +0800

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export const metadata = {
   title: "Kyle's Cookbook",
   description: "Personal Kitchen Lab",
   manifest: "/manifest.json", // 链接 manifest
+
   themeColor: "#FFFFFF",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
   appleWebApp: {
